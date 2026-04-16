e62fc9958c4cfa1f111e57e344f5fc1ecee4b579
Sebastian <sebashm68@gmail.com>
2026-04-16 00:08:14 -0600

    Añade form de comida

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import RegisterDay from './components/RegisterDay.jsx';
 import DailyTotal from './components/DailyTotal.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header.jsx';
+import { addLunchTime } from './services/api.js';
+import TimeTrackingForm from './components/TimeTrackingForm.jsx';
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Form />}/>
           <Route path='/registerDay' element={<RegisterDay />}/>
+          <Route path='/registerFood' element={<TimeTrackingForm title="Registro de hora de comida" functionApi={addLunchTime}/>}/>
           <Route path='/dailyTotal' element={<DailyTotal />}/>
         </Routes>
       </Router> 
--- src/services/api.js
+++ src/services/api.js
@@ -58,6 +58,24 @@ export async function readSale() {
     return respuesta;
     
 }
+export async function addLunchTime(data) {
+    const { nombre, access, fecha, hora } = data;
+
+    try {
+        const respuesta = await store
+        .append(`comida-${access.toLowerCase()}`, [
+            {
+                'Nombre': nombre,
+                'Fecha' : fecha,
+                [access] : hora,
+            }
+        ]);
+        return respuesta;
+        
+    } catch (error) {
+        return error;   
+    }
+}
 
 
 
